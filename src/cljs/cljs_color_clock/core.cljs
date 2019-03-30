a56e11eb5addd4081e90a4ad1060c4c27c6dc9c4
Pablo Jurado <pjurado85@gmail.com>
2019-03-30 13:53:53 -0500

    change data structure

--- src/cljs/cljs_color_clock/core.cljs
+++ src/cljs/cljs_color_clock/core.cljs
@@ -2,6 +2,8 @@
   (:require
    [reagent.core :as reagent :refer [atom]]))
 
+(def state (atom {:time {:hour 10 :min 5 :sec 05} :show-hex false}))
+
 (defn log [& arg]
   (js/console.log arg.arr))
 
@@ -14,23 +16,28 @@
 (defn seconds [t]
   (.getSeconds t))
 
-(defn get-time [t]
-  (str (hour t) ":" (minutes t) ":" (seconds t)))
-
-(def my-atom (atom (get-time (js/Date.))))
-
-(js/setInterval #(reset! my-atom (get-time (js/Date.))) 1000)
+(defn update-state []
+  ((reset! state (assoc-in @state [:time :hour] (hour (js/Date.))))
+   (reset! state (assoc-in @state [:time :min] (minutes (js/Date.))))
+   (reset! state (assoc-in @state [:time :sec] (seconds (js/Date.))))))
 
 (defn in []
-  (log "in"))
+  (reset! state (assoc @state :show-hex true)))
 
 (defn out []
-  (log "out"))
+  (reset! state (assoc @state :show-hex false)))
 
 (defn clock []
-  [:span {
-    :on-mouse-over in
-    :on-mouse-out out} @my-atom])
+  (def hex (get @state :show-hex))
+  [:span {:on-mouse-over in :on-mouse-out out}
+    [:div (str hex)]
+    [:span  (get-in @state [:time :hour])]
+    [:span ":"]
+    [:span  (get-in @state [:time :min])]
+    [:span ":"]
+    [:span  (get-in @state [:time :sec])]])
+
+(js/setInterval update-state 1000)
 
 (defn app []
   [:div
